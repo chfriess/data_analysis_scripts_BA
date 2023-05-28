c4854a5ca787cba62272a552fd177c8600f69f6b
Christian Frieß <Ch.Friess@campus.lmu.de>
2023-05-28 15:19:32 +0200

    synching em and displacement based on upstroke: improved detection of displacement upstroke

--- synch_em_coregistration_with_bioelectric_data.py
+++ synch_em_coregistration_with_bioelectric_data.py
@@ -31,30 +31,38 @@ def find_signal_deviations_stft(signal):
 
     plt.show()
 
+def find_point_of_first_upstroke_displacement(signal) -> int:
+    std = np.std(signal[:10]) * 5
 
-def find_point_of_first_upstroke(signal) -> int:
+    n = 0
+    for i in range(1, len(signal)):
+        if signal[i] - signal[i - 1] > std:
+            n = i - 1
+            break
+    return n
+
+def find_point_of_first_upstroke_em(signal) -> int:
     signal_derivative = np.gradient(signal)
-
     signal_rolling_variance = pd.Series(signal_derivative).rolling(window=10, center=False).std().to_numpy()
-
     offset = 0
     for i, el in enumerate(signal_rolling_variance):
         if not math.isnan(el):
             offset = i
             break
 
-    std = np.std(signal_rolling_variance[offset:offset + 10]) * 10
+    std = np.std(signal_rolling_variance[offset:offset + 20]) * 20
 
     n = 0
     for i in range(offset + 1, len(signal_rolling_variance)):
         if signal_rolling_variance[i] - signal_rolling_variance[i - 1] > std:
             n = i - 1
             break
+    print(n)
     return n
 
 
 if __name__ == '__main__':
-    for COREGISTRATION_NR in [str(x) for x in range(30, 31)]:
+    for COREGISTRATION_NR in [str(x) for x in range(27, 28)]:
         DESTINATION = "C:\\Users\\Chris\\OneDrive\\Desktop\\phantom coregistration data\\06_05_2023_BS\\coregistration_" + COREGISTRATION_NR + "\\em_groundtruth\\groundtruth_" + COREGISTRATION_NR + "_cropped.npy"
         em_groundtruth_raw = np.load(
             "C:\\Users\\Chris\\OneDrive\\Desktop\\phantom coregistration data\\06_05_2023_BS\\coregistration_" + COREGISTRATION_NR + "\\em_groundtruth\\displacement_from_origin.npy")
@@ -71,15 +79,16 @@ if __name__ == '__main__':
             "C:\\Users\\Chris\\OneDrive\\Desktop\\phantom coregistration data\\06_05_2023_BS\\coregistration_" + COREGISTRATION_NR + "\\data_bioelectric_sensors\\cumulative displacements sample" + COREGISTRATION_NR + ".npy")
         cumulative_displacement = cumulative_displacement[:len(cumulative_displacement) - 5]
 
-        displacement_upstroke = find_point_of_first_upstroke(cumulative_displacement)
-        em_upstroke = find_point_of_first_upstroke(em_groundtruth_raw)
+        displacement_upstroke = find_point_of_first_upstroke_displacement(cumulative_displacement)
+
+        em_upstroke = find_point_of_first_upstroke_em(em_groundtruth_raw)
         em_start = em_upstroke-(displacement_upstroke*3)
 
         em_before_start = em_groundtruth_raw[em_start:em_upstroke:3]
         em_after_start = em_groundtruth_raw[em_upstroke+1:em_upstroke+1+(len(cumulative_displacement)-displacement_upstroke)*3:3]
 
         em_cropped = np.append(em_before_start,em_after_start)
-
+        
         plt.plot(cumulative_displacement+em_groundtruth_raw[0], color="green")
         plt.plot(em_cropped, color="blue")
         plt.axvline(x=displacement_upstroke, color='r', label="timestamp synchronization point")
