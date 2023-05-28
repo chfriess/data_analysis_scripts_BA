c6c83ad5579fcdc8e0fe5d669a45cd660a3c654d
Christian Frieß <Ch.Friess@campus.lmu.de>
2023-05-28 15:04:37 +0200

    synching em and dipslacemet sketch by upstroke detection

--- extract_displacement_and_bioelectric_data.py
+++ extract_displacement_and_bioelectric_data.py
@@ -6,75 +6,120 @@ from matplotlib import pyplot as plt
 NO = "35"
 
 if __name__ == "__main__":
-    os.chdir("C:\\Users\\Chris\\OneDrive\\Desktop\\phantom coregistration data\\06_05_2023_BS\\")
-
-    aorta = [1/20 for x in range(200)]
-    iliaca = [1/13 for x in range(90)]
-
-    groundtruth = np.array(aorta + iliaca)
-
-    plt.plot(groundtruth)
-    plt.savefig("groundtruth.svg")
-    np.save("groundtruth.npy", groundtruth)
 
 
+    for COREGISTRATION_NR in [str(x) for x in range(25, 26)]:
+        path = "C:\\Users\\Chris\\OneDrive\\Desktop\\phantom coregistration data\\06_05_2023_BS\\coregistration_" + COREGISTRATION_NR + "\\data_bioelectric_sensors\\"
+        DESTINATION = "C:\\Users\\Chris\\OneDrive\\Desktop\\phantom coregistration data\\06_05_2023_BS\\coregistration_" + COREGISTRATION_NR + "\\em_groundtruth\\groundtruth_" + COREGISTRATION_NR + "_cropped.npy"
+
+        displacements = np.load(path + "cumulative displacements sample" + COREGISTRATION_NR + ".npy")
+        impedance = np.load(path + "impedance_sample" + COREGISTRATION_NR + ".npy")
+
+        em_groundtruth = np.load(DESTINATION)
+
+        plt.plot(displacements)
+        #plt.plot(impedance)
+        #plt.plot(em_groundtruth)
+
+        plt.show()
 
 
 
-    os.chdir("C:\\Users\\Chris\\OneDrive\\Desktop\\phantom coregistration data\\06_05_2023_BS\\coregistration_"+NO+"\\data_bioelectric_sensors\\")
-    FILE = "coregistration_"+NO+"_sampling_corrected.npz"
+"""
     
-    
+    displacements = np.load(path+"displacements_sample"+COREGISTRATION_NR+".npy")
+    impedance = np.load(path + "impedance_sample" + COREGISTRATION_NR + ".npy")
 
-    data = np.load(FILE)
+    if len(displacements) > len(impedance):
+        displacements = displacements[1:]
 
-    # DISPLACEMENTS
-    cumulative_displacements = data["windows_x_translation"]
-    impedance = data["fft_magnitude_BminA"]
-    print(len(cumulative_displacements))
-    print(len(impedance))
-    print(cumulative_displacements[1])
+    np.save(path+"displacements_sample"+COREGISTRATION_NR+".npy", displacements)
 
-    """
-    displacements = []
+    displacements = np.load(path + "displacements_sample" + COREGISTRATION_NR + ".npy")
+    impedance = np.load(path + "impedance_sample" + COREGISTRATION_NR + ".npy")
 
-    for i in range(1, len(cumulative_displacements)):
-        displacements.append(cumulative_displacements[i] - cumulative_displacements[i-1])
+    em_groundtruth = np.load(DESTINATION)
 
+    if len(displacements) != len(impedance):
+        raise ValueError("Now impedance and displacements must share the same length")
 
-    plt.plot(cumulative_displacements)
-    plt.title("cumulative displacement sample" + NO)
-    plt.savefig("cumulative_displacement_sample" + NO + ".svg")
-
-    plt.clf()
-    plt.plot(displacements)
-    plt.title("displacements sample" + NO)
-    plt.savefig("displacements_sample" + NO + ".svg")
+    if len(displacements) != len(em_groundtruth):
+        raise ValueError("Now displacements and groundtruth must share same length")
 
 
 
+os.chdir("C:\\Users\\Chris\\OneDrive\\Desktop\\phantom coregistration data\\06_05_2023_BS\\")
 
-    np.save("cumulative displacements sample" + NO + ".npy", cumulative_displacements)
-    np.save("displacements_sample" + NO + ".npy", displacements)
+aorta = [1/20 for x in range(200)]
+iliaca = [1/13 for x in range(90)]
+
+groundtruth = np.array(aorta + iliaca)
+
+plt.plot(groundtruth)
+plt.savefig("groundtruth.svg")
+np.save("groundtruth.npy", groundtruth)
 
 
 
 
-    #IMPEDANCE
 
-    impedance = data["fft_magnitude_BminA"]
+os.chdir("C:\\Users\\Chris\\OneDrive\\Desktop\\phantom coregistration data\\06_05_2023_BS\\coregistration_"+NO+"\\data_bioelectric_sensors\\")
+FILE = "coregistration_"+NO+"_sampling_corrected.npz"
 
-    plt.clf()
-    plt.plot(impedance)
-    plt.title("impedance sample" + NO)
-    plt.show()
-    plt.savefig("impedance_sample" + NO + ".svg")
 
-    np.save("impedance_sample" + NO + ".npy", impedance)
 
-    data.close()
-    
-    
-    """
+data = np.load(FILE)
+
+
+
+
+# DISPLACEMENTS
+cumulative_displacements = data["windows_x_translation"]
+impedance = data["fft_magnitude_BminA"]
+print(len(cumulative_displacements))
+print(len(impedance))
+print(cumulative_displacements[1])
+
+
+displacements = []
+
+for i in range(1, len(cumulative_displacements)):
+    displacements.append(cumulative_displacements[i] - cumulative_displacements[i-1])
+
+
+plt.plot(cumulative_displacements)
+plt.title("cumulative displacement sample" + NO)
+plt.savefig("cumulative_displacement_sample" + NO + ".svg")
+
+plt.clf()
+plt.plot(displacements)
+plt.title("displacements sample" + NO)
+plt.savefig("displacements_sample" + NO + ".svg")
+
+
+
+
+np.save("cumulative displacements sample" + NO + ".npy", cumulative_displacements)
+np.save("displacements_sample" + NO + ".npy", displacements)
+
+
+
+
+#IMPEDANCE
+
+impedance = data["fft_magnitude_BminA"]
+
+plt.clf()
+plt.plot(impedance)
+plt.title("impedance sample" + NO)
+plt.show()
+plt.savefig("impedance_sample" + NO + ".svg")
+
+np.save("impedance_sample" + NO + ".npy", impedance)
+
+data.close()
+
+
+"""
 
 
--- synch_em_coregistration_with_bioelectric_data.py
+++ synch_em_coregistration_with_bioelectric_data.py
@@ -1,8 +1,60 @@
+import math
+import statistics
+
 import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt
+from scipy.signal import stft, find_peaks
+
+
+def moving_average(interval, window_size):
+    window = np.ones(int(window_size)) / float(window_size)
+    return np.convolve(interval, window, 'same')
+
+
+def find_signal_deviations_with_moving_average(signal):
+    signal_moving_average = list(moving_average(signal, 5))
+
+    std_signal = np.std(signal_moving_average)
+    events = []
+    indices = []
+    for i in range(len(signal)):
+        if signal[i] > std_signal + std_signal:
+            events.append(signal[i])
+            indices.append(i)
+    return indices, events
+
+
+def find_signal_deviations_stft(signal):
+    f, t, Zxx = stft(signal, nperseg=1000)
+    plt.pcolormesh(t, f, np.abs(Zxx), vmin=0, vmax=2, shading='gouraud')
+
+    plt.show()
+
+
+def find_point_of_first_upstroke(signal) -> int:
+    signal_derivative = np.gradient(signal)
+
+    signal_rolling_variance = pd.Series(signal_derivative).rolling(window=10, center=False).std().to_numpy()
+
+    offset = 0
+    for i, el in enumerate(signal_rolling_variance):
+        if not math.isnan(el):
+            offset = i
+            break
+
+    std = np.std(signal_rolling_variance[offset:offset + 10]) * 10
+
+    n = 0
+    for i in range(offset + 1, len(signal_rolling_variance)):
+        if signal_rolling_variance[i] - signal_rolling_variance[i - 1] > std:
+            n = i - 1
+            break
+    return n
+
 
 if __name__ == '__main__':
-    for COREGISTRATION_NR in [str(x) for x in range(25, 36)]:
+    for COREGISTRATION_NR in [str(x) for x in range(30, 31)]:
         DESTINATION = "C:\\Users\\Chris\\OneDrive\\Desktop\\phantom coregistration data\\06_05_2023_BS\\coregistration_" + COREGISTRATION_NR + "\\em_groundtruth\\groundtruth_" + COREGISTRATION_NR + "_cropped.npy"
         em_groundtruth_raw = np.load(
             "C:\\Users\\Chris\\OneDrive\\Desktop\\phantom coregistration data\\06_05_2023_BS\\coregistration_" + COREGISTRATION_NR + "\\em_groundtruth\\displacement_from_origin.npy")
@@ -15,6 +67,24 @@ if __name__ == '__main__':
             "C:\\Users\\Chris\\OneDrive\\Desktop\\phantom coregistration data\\06_05_2023_BS\\coregistration_" + COREGISTRATION_NR + "\\coregistration_" + COREGISTRATION_NR + "_em.csv",
             sep='\t')
         t_em = timestamps_em.iloc[:, 16].tolist()
+        cumulative_displacement = np.load(
+            "C:\\Users\\Chris\\OneDrive\\Desktop\\phantom coregistration data\\06_05_2023_BS\\coregistration_" + COREGISTRATION_NR + "\\data_bioelectric_sensors\\cumulative displacements sample" + COREGISTRATION_NR + ".npy")
+        cumulative_displacement = cumulative_displacement[:len(cumulative_displacement) - 5]
+
+        displacement_upstroke = find_point_of_first_upstroke(cumulative_displacement)
+        em_upstroke = find_point_of_first_upstroke(em_groundtruth_raw)
+        em_start = em_upstroke-(displacement_upstroke*3)
+
+        em_before_start = em_groundtruth_raw[em_start:em_upstroke:3]
+        em_after_start = em_groundtruth_raw[em_upstroke+1:em_upstroke+1+(len(cumulative_displacement)-displacement_upstroke)*3:3]
+
+        em_cropped = np.append(em_before_start,em_after_start)
+
+        plt.plot(cumulative_displacement+em_groundtruth_raw[0], color="green")
+        plt.plot(em_cropped, color="blue")
+        plt.axvline(x=displacement_upstroke, color='r', label="timestamp synchronization point")
+
+        """
 
         starting_point = 0
         for i in range(len(t_em)):
@@ -22,12 +92,28 @@ if __name__ == '__main__':
                 starting_point = i
                 break
 
+        x = [z for z in range(starting_point, starting_point+len(cumulative_displacement))]
         em_groundtruth_cropped = []
-
         for i in range(starting_point, starting_point + len(t_bio) * 3, 3):
             em_groundtruth_cropped.append(em_groundtruth_raw[i])
 
         if (len(em_groundtruth_cropped) != len(t_bio)):
             raise ValueError("after cropping length of groundtruth must be equal to length of bioelectric signal")
 
-        np.save(DESTINATION, np.array(em_groundtruth_cropped))
+
+        #print(len(em_groundtruth_raw))
+        #print(len(timestamps_em))
+
+        plt.plot(em_groundtruth_raw[:2000], label="cumulative em groundtruth")
+
+        plt.axvline(x=starting_point, color='r', label="timestamp synchronization point")
+        plt.plot(x, cumulative_displacement, label="cumulative displacement")
+        plt.ylabel("position along centerline in mm")
+        plt.xlabel("update step")
+        plt.legend()
+        plt.show()
+
+        #plt.plot(em_groundtruth_cropped, color="red")
+
+        #np.save(DESTINATION, np.array(em_groundtruth_cropped))
+"""
