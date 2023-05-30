307d772e576b8afebd74fb14a5f2f3be2caa43ff
Christian Frieß <Ch.Friess@campus.lmu.de>
2023-05-30 14:39:12 +0200

    statistical evaluation finished

--- statistical_evaluation.py
+++ statistical_evaluation.py
@@ -1,23 +1,52 @@
-# Import library
-import numpy as np
 import pandas as pd
-from statsmodels.formula.api import ols
-from statsmodels.multivariate.manova import MANOVA
-from statsmodels.stats.multicomp import pairwise_tukeyhsd
+from scipy import stats
 from statsmodels.stats.anova import AnovaRM
 
-# Create the data
+number_of_anovas = 1
+
+def post_hoc_test_RM_bonferroni(group_one: str, group_two: str, nr_of_comparisons: int, alpha: float):
+    level_of_significance = alpha / nr_of_comparisons
+    post_hoc_result = stats.ttest_rel(assumptions[group_one], assumptions[group_two])
+    is_significant = post_hoc_result[1] < level_of_significance
+    print(group_one + " vs. " + group_two+ ": " + str(post_hoc_result) +
+          " |  is significant with level " + str(level_of_significance) + ": " + str(is_significant))
+    print("\n")
 
 
-dataframe = pd.DataFrame({'Cars': np.repeat([1, 2, 3, 4, 5], 4),
-                          'Oil': np.tile([1, 2, 3, 4], 5),
-                          'Mileage': [36, 38, 30, 29,
-                                      34, 38, 30, 29,
-                                      34, 28, 38, 32,
-                                      38, 34, 20, 44,
-                                      26, 28, 34, 50],
-                          })
-print(dataframe)
-#print(AnovaRM(data=dataframe, depvar='Mileage',
- #             subject='Cars', within=['Oil']).fit())
+df = pd.read_csv("C:\\Users\\Chris\\OneDrive\\Desktop\\stats_playground_agar.csv")
+assumptions = pd.read_csv("C:\\Users\\Chris\\OneDrive\\Desktop\\stats_playground_visualization.csv")
+print("Descriptive statistics: ")
+print(assumptions.describe())
+print("\n\n")
+
+print("Testing the assumptions:")
+print("Testing normal distribution of the samples: ")
+shapiro_test_ahistoric = stats.shapiro(assumptions["ahistoric"])
+print("shapiro test for ahistoric: " + str(shapiro_test_ahistoric ))
+shapiro_test_sliding_dtw = stats.shapiro(assumptions["sliding_dtw"])
+print("shapiro test for sliding_dtw: " + str(shapiro_test_sliding_dtw))
+shapiro_test_displacement = stats.shapiro(assumptions["displacement"])
+print("shapiro test for displacement: " + str(shapiro_test_displacement))
+shapiro_test_alpha_displacement = stats.shapiro(assumptions["alpha_displacement"])
+print("shapiro test for alpha_displacement: " + str(shapiro_test_alpha_displacement ))
+print("\n")
+
+print("testing homogeneity of variance of the four groups: ")
+levene = stats.levene(assumptions["ahistoric"], assumptions["sliding_dtw"], assumptions["displacement"], assumptions["alpha_displacement"])
+print(levene)
+
+
+
+print("\nConduction the repeated measures anova for the four samples: ")
+anovaRM = AnovaRM(data=df, depvar="rms", subject="sample", within=["measurement"]).fit()
+print(anovaRM)
+if anovaRM.anova_table["Pr > F"][0] < 0.05/number_of_anovas:
+    print("ANOVA was significant with level of significance" + str(0.05/number_of_anovas))
+    # Bonferroni corrected post hoc significance level
+
+
+    print("\nConduction the poshoc tests as related-sample t-tests with bonferroni corrected p-value: ")
+    for i in range(len(assumptions.columns)-1):
+        for j in range(i, len(assumptions.columns)):
+            post_hoc_test_RM_bonferroni(assumptions.columns[i], assumptions.columns[j], 6, 0.05)
 
--- synch_em_coregistration_with_bioelectric_data.py
+++ synch_em_coregistration_with_bioelectric_data.py
@@ -1,10 +1,8 @@
 import math
-import statistics
-
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
-from scipy.signal import stft, find_peaks
+from scipy.signal import stft
 
 
 def moving_average(interval, window_size):
@@ -31,6 +29,7 @@ def find_signal_deviations_stft(signal):
 
     plt.show()
 
+
 def find_point_of_first_upstroke_displacement(signal) -> int:
     std = np.std(signal[:10]) * 5
 
@@ -41,6 +40,7 @@ def find_point_of_first_upstroke_displacement(signal) -> int:
             break
     return n
 
+
 def find_point_of_first_upstroke_em(signal) -> int:
     signal_derivative = np.gradient(signal)
     signal_rolling_variance = pd.Series(signal_derivative).rolling(window=10, center=False).std().to_numpy()
@@ -50,7 +50,7 @@ def find_point_of_first_upstroke_em(signal) -> int:
             offset = i
             break
 
-    std = np.std(signal_rolling_variance[offset:offset + 20]) * 20
+    std = np.std(signal_rolling_variance[offset:offset + 20]) * 25
 
     n = 0
     for i in range(offset + 1, len(signal_rolling_variance)):
@@ -62,67 +62,28 @@ def find_point_of_first_upstroke_em(signal) -> int:
 
 
 if __name__ == '__main__':
-    for COREGISTRATION_NR in [str(x) for x in range(27, 28)]:
+    for COREGISTRATION_NR in [str(x) for x in [34]]:
         DESTINATION = "C:\\Users\\Chris\\OneDrive\\Desktop\\phantom coregistration data\\06_05_2023_BS\\coregistration_" + COREGISTRATION_NR + "\\em_groundtruth\\groundtruth_" + COREGISTRATION_NR + "_cropped.npy"
         em_groundtruth_raw = np.load(
             "C:\\Users\\Chris\\OneDrive\\Desktop\\phantom coregistration data\\06_05_2023_BS\\coregistration_" + COREGISTRATION_NR + "\\em_groundtruth\\displacement_from_origin.npy")
-
-        timestamps_bioelectric = pd.read_csv(
-            "C:\\Users\\Chris\\OneDrive\\Desktop\\phantom coregistration data\\06_05_2023_BS\\coregistration_" + COREGISTRATION_NR + "\\data_bioelectric_sensors\\coregistration_" + COREGISTRATION_NR + "__magnitudes_and_timestamps.csv")
-        t_bio = timestamps_bioelectric[' timestamps'].tolist()
-
-        timestamps_em = pd.read_csv(
-            "C:\\Users\\Chris\\OneDrive\\Desktop\\phantom coregistration data\\06_05_2023_BS\\coregistration_" + COREGISTRATION_NR + "\\coregistration_" + COREGISTRATION_NR + "_em.csv",
-            sep='\t')
-        t_em = timestamps_em.iloc[:, 16].tolist()
         cumulative_displacement = np.load(
             "C:\\Users\\Chris\\OneDrive\\Desktop\\phantom coregistration data\\06_05_2023_BS\\coregistration_" + COREGISTRATION_NR + "\\data_bioelectric_sensors\\cumulative displacements sample" + COREGISTRATION_NR + ".npy")
-        cumulative_displacement = cumulative_displacement[:len(cumulative_displacement) - 5]
+        cumulative_displacement = cumulative_displacement[:len(cumulative_displacement) - 2]
 
         displacement_upstroke = find_point_of_first_upstroke_displacement(cumulative_displacement)
 
         em_upstroke = find_point_of_first_upstroke_em(em_groundtruth_raw)
-        em_start = em_upstroke-(displacement_upstroke*3)
+        em_start = em_upstroke - (displacement_upstroke * 3)
 
         em_before_start = em_groundtruth_raw[em_start:em_upstroke:3]
-        em_after_start = em_groundtruth_raw[em_upstroke+1:em_upstroke+1+(len(cumulative_displacement)-displacement_upstroke)*3:3]
+        em_after_start = em_groundtruth_raw[
+                         em_upstroke + 1:em_upstroke + 1 + (len(cumulative_displacement) - displacement_upstroke) * 3:3]
 
-        em_cropped = np.append(em_before_start,em_after_start)
-        
-        plt.plot(cumulative_displacement+em_groundtruth_raw[0], color="green")
+        em_cropped = np.append(em_before_start, em_after_start)
+
+        plt.plot(cumulative_displacement + em_groundtruth_raw[0], color="green")
         plt.plot(em_cropped, color="blue")
         plt.axvline(x=displacement_upstroke, color='r', label="timestamp synchronization point")
+        #plt.show()
 
-        """
-
-        starting_point = 0
-        for i in range(len(t_em)):
-            if t_em[i] > t_bio[0]:
-                starting_point = i
-                break
-
-        x = [z for z in range(starting_point, starting_point+len(cumulative_displacement))]
-        em_groundtruth_cropped = []
-        for i in range(starting_point, starting_point + len(t_bio) * 3, 3):
-            em_groundtruth_cropped.append(em_groundtruth_raw[i])
-
-        if (len(em_groundtruth_cropped) != len(t_bio)):
-            raise ValueError("after cropping length of groundtruth must be equal to length of bioelectric signal")
-
-
-        #print(len(em_groundtruth_raw))
-        #print(len(timestamps_em))
-
-        plt.plot(em_groundtruth_raw[:2000], label="cumulative em groundtruth")
-
-        plt.axvline(x=starting_point, color='r', label="timestamp synchronization point")
-        plt.plot(x, cumulative_displacement, label="cumulative displacement")
-        plt.ylabel("position along centerline in mm")
-        plt.xlabel("update step")
-        plt.legend()
-        plt.show()
-
-        #plt.plot(em_groundtruth_cropped, color="red")
-
-        #np.save(DESTINATION, np.array(em_groundtruth_cropped))
-"""
+        np.save(DESTINATION, np.array(em_cropped))
